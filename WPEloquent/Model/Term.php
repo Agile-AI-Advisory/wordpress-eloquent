efd1ffded0fb6ae47154741bf3b7e2b4709164e9
James <james.hayes@purrgroup.com>
2022-11-15 10:01:33 +0000

    try a new way

---
+++ WPEloquent/Model/Term.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace WPEloquent\Model;
+
+use \WPEloquent\Traits\HasMeta;
+
+class Term extends \Illuminate\Database\Eloquent\Model
+{
+
+    use HasMeta;
+
+    protected $table = 'terms';
+    protected $primaryKey = 'term_id';
+    protected $guarded = [];
+    public $timestamps = false;
+
+    public function meta()
+    {
+        return $this->hasMany(\WPEloquent\Model\Term\Meta::class, 'term_id')
+            ->select(['term_id', 'meta_key', 'meta_value']);
+    }
+
+}
